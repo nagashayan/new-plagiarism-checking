33b5fef56c18ccc80b4a5fdace3d3047707025d3
Nagashayan <nagashayan1@gmail.com>
2018-01-30 13:54:53 -0500

    changed app name

--- plagarism_checker/plagarism_checker/urls.py
+++ plagarism_checker/plagarism_checker/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from checker import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^checker/$', views.processInput),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^plagiarism-checker/$', views.processInput),
 ]
